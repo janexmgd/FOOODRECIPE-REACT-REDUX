e22262118f379f0f6140b757dd57aa9b71a95a7a
Denny Wahyu <janexmgd@gmail.com>
2022-07-01 11:26:04 +0700

    remove logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import promiseMiddleware from "redux-promise-middleware";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
