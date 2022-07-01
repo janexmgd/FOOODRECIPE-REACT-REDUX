2ce9dc86552364405e8d31d36693d811b832d644
Denny Wahyu <janexmgd@gmail.com>
2022-07-01 12:37:34 +0700

    fix message delete recipe

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -37,7 +37,6 @@ const Index = () => {
 		if (token) {
 			dispatch(getDetailUser());
 		}
-		console.log(latestRecipe.isLoading);
 	}, []);
 	return (
 		<>
--- src/redux/actions/deleteRecipe.js
+++ src/redux/actions/deleteRecipe.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 const token = localStorage.getItem("token");
 export const onDelete = (id) => {
-  return new Promise((resolve, reject) => {
-    axios
-      .delete(`${process.env.REACT_APP_MY_BACKEND}/recipe/${id}`, {
-        headers: {
-          token: token,
-        },
-      })
-      .then((res) => {
-        resolve(res.data.data);
-      })
-      .catch((err) => {
-        reject(err.data.data);
-      });
-  });
+	return new Promise((resolve, reject) => {
+		axios
+			.delete(`${process.env.REACT_APP_MY_BACKEND}/recipe/${id}`, {
+				headers: {
+					token: token,
+				},
+			})
+			.then((res) => {
+				resolve(res.data);
+			})
+			.catch((err) => {
+				reject(err.data);
+			});
+	});
 };
